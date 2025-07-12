14b5b70f0af1869015ecf5140d97b241df787bf5
MarkJun139 <dnflskfk0200@naver.com>
2025-07-12 22:35:10 +0900

    실행까지 OK

--- src/scripts/puppeteer-runner.ts
+++ src/scripts/puppeteer-runner.ts
@@ -4,19 +4,65 @@ import { webkit } from 'playwright';
 import puppeteer from 'puppeteer-extra';
 import stealth from 'puppeteer-extra-plugin-stealth';
 
+import chromeLauncher from 'chrome-launcher';
+
+import { connect } from "puppeteer-real-browser";
+
+
 import fs from 'fs';
 
-puppeteer.use(stealth());
+// puppeteer.use(stealth());
 
 export async function runAutoLogin() {
-
-  const userDataDir = 'C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data';
-  const browser = await chromium.launchPersistentContext(userDataDir, {
+  console.log('🔥 시작됨');
+  const {page, browser} = await connect({
     headless: false,
-    args: ['--start-maximized'],
+
+    args: [],
+
+    customConfig: {
+      userDataDir: './user_data'
+    },
+
+    turnstile: true,
+
+    connectOption: {},
+
+    disableXvfb: false,
+
+    ignoreAllFlags: false,
+
+
   });
-  const page = await browser.newPage();
-  await page.goto('https://www.ticketlink.co.kr');
+
+
+  console.log('👉 페이지 연결 완료');
+
+  // const newPage = await browser.newPage();
+  // const browser = connection.browser;
+  // const newPage = await browser.newPage(); 
+
+  await page.goto('https://www.ticketlink.co.kr', {
+
+  });
+
+  console.log('✅ 페이지 이동 성공');
+  // console.log('페이지 제목:', await page.title());
+
+  
+
+  // 3초 기다린 뒤 브라우저 종료
+  // await page.waitForTimeout(3000);
+  // await browser.close();
+
+    // const userDataDir = 'C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data';
+    // const browser = await chromium.launchPersistentContext(userDataDir, {
+    //   headless: false,
+    //   args: ['--start-maximized'],
+  // });
+  // const page = await browser.newPage();
+  // await page.goto('https://www.ticketlink.co.kr');
+
 
 
 }
